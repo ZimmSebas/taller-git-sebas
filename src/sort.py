38e3217fb777dd13ed2c9daf234d8d8624d85002
sebamoralesasd <scmsasd@gmail.com>
2023-05-18 17:30:45 -0300

    Primer commit taller 2023

--- src/sort.py
+++ src/sort.py
@@ -2,11 +2,12 @@ def reversa(lista):
     lista.sort(reverse=True)
     return lista
 
-pepe = [3, 1, 2, 4, 123, 51]
+pepe = [3, 123, 51]
 
 pepe.sort()
 print(f"pepe: {pepe}")
+print(f"pepe: {reversa(pepe)}")
 
 # Orden de mayor a menor
-epep = reversa(pepe)
-print(f"pepe: {epep}")
+# epep = reversa(pepe)
+# print(f"pepe: {epep}")
